192f549ebf727d59c2a028affa6bccbe8f36d0cb
yangsai <yangsaipp@qq.com>
2016-08-03 23:19:55 +0800

    修改build.gradle，配置eclipse中java、resource的output与gradle java插件的output一致，方便eclipse开发调试

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class DemoApplication {
